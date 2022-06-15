29d608a679deb0dd525f455c8aea34cede79dd7a
andre1670 <36558814+andre1670@users.noreply.github.com>
2022-06-15 21:15:15 +0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,5 +1,5 @@
 import plotly.express as px
 
 df = px.read_csv('https://raw.githubusercontent.com/andre1670/Sumo-heya-rikishi/main/active.csv')
-fig = px.line(df, x="Basho", y="Rikishi", title='dosukoi')
+fig = px.line(df, x="Basho", y="Rikishi", title='Dewanoumi')
 fig.show()
